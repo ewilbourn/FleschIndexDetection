075f59ba559209f2b93abc1a6ddb8e71991effae
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-03 11:54:04 -0400

    trying to read in files in Java

--- java/flesch.java
+++ java/flesch.java
@@ -1,3 +1,29 @@
-//Emily Wilbourn\
+//Emily Wilbourn
 //Flesch program in Java
+import java.io.*;
+import java.util.*;
+public class flesch
+{
+	public static void main(String[] args) throws IOException
+	{
+		Scanner in = new Scanner (System.in);
+		System.out.print("Enter the name of the input file: ");
+		String inputFile = in.nextLine();
+		//System.out.print(inputFile);
+		readFiles(inputFile);	
+	}	
+
+	public static void readFiles(String input) throws FileNotFoundException
+	{
+        	Scanner in = new Scanner(new File(input));
+        	System.out.println("Hello.");
+		while (in.hasNextLine())
+        	{
+                	System.out.println("I am in the while loop.");
+			System.out.println(in.nextLine());
+        	}
+		System.out.println("Bye.");
+	}
+
+}
 
