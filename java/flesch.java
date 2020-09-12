f123b85a11cfd130d9e54c6dfc8aef1e48955b28
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-12 16:04:09 -0400

    added method to determine if a token is a number, however it isn't catching all numbers.

--- java/flesch.java
+++ java/flesch.java
@@ -15,7 +15,7 @@ public class flesch
 		
 		ArrayList <String> words = new ArrayList <String>();	
 		words = findWords(words, inputFile);
-
+		System.out.println(words.size());
 		for (int i = 0; i < words.size(); i++)
 		{
 			System.out.println(words.get(i));
@@ -34,17 +34,12 @@ public class flesch
         	Scanner sc = new Scanner(new File(input));
 		while(sc.hasNext())
         	{
-
-        		try 
+			//if the word, sc.next(), is not a number, add it to the ArrayList
+			if(!isNumber(sc.next()))
 			{
-           			Double.parseDouble(sc.next());
-        		} 
-			catch (NumberFormatException e) 
-			{
-				//if the word, sc.next(), is not a number, add it to the ArrayList
 				words.add(sc.next());
 			}	
-		}
+	}	
 
 		sc.close();
 		return words;
@@ -53,12 +48,25 @@ public class flesch
 	//add method to determine if a char is a vowel
 	
 
-	//add method to determine if a word is actually a number
-	
+	//method to determine if a word is actually a number
+	//precondition: passes in a string
+	//postcondition: returns a boolean that tells us if the string is a number
+	static boolean isNumber(String str)
+	{	
+		int counter = 0;
+		
+		//loop that increments counter when the character is a digit
+		for (int i = 0; i < str.length(); i++)
+			if (Character.isDigit(str.charAt(i)) == true)
+				counter += 1;
+		//if counter equals str.lenght(), it means that every char in the 
+		//string is a digit, which means that we have a number
+		return (counter == str.length() ? true : false); 
+	}	
 
 	//add method to determine how many syllables a word has (will need to call vowel method)
 	//probably need to look at each word and then see where the vowels are and count the number of vowels? 
 	//but if vowels are next to each other, they don't make up two different syllables
 	
-}
+} 
 
