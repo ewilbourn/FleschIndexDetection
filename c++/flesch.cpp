8833ec92cee09e686d77bb4f04e847d1d22d099c
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-21 11:56:50 -0400

    Only adding words (not numbers) to my deque now

--- c++/flesch.cpp
+++ c++/flesch.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <deque>
+#include <stdio.h>
+#include <ctype.h>
+
 using namespace std;
-void readFile(deque<string> &words, string input);
+void getWords(deque<string> &words, string input);
+bool isNumber(string str);
+
 int main ()
 {
 	string input;
@@ -11,12 +16,12 @@ int main ()
 
 	deque<string> words(1);
 
-	readFile(words, input);
-	//for (int i = 1; i < words.size(); i++)
-	//	cout << words[i] << endl;
+	getWords(words, input);
+	for (int i = 1; i < words.size(); i++)
+		cout << words[i] << endl;
 	return 0;
 }
-void readFile(deque<string> &words, string input)
+void getWords(deque<string> &words, string input)
 {
     ifstream file;
     file.open (input);
@@ -25,6 +30,21 @@ void readFile(deque<string> &words, string input)
     string word;
     while (file >> word)
     {
-        words.push_back(word);
+ 	if(!isNumber(word))
+	       words.push_back(word);
     }
+}
+
+bool isNumber (string str)
+{
+	int counter = 0;
+	
+	//loop that increments the counter whtn the character is a digit
+	for (int i = 0; i < str.size(); i++)
+		if(isdigit(str[i]))
+			counter++;
+	
+	//if the counter equals the size of the string, then the value
+	//being stored in the string is a digit
+	return (counter == str.size() ? true : false);
 }
