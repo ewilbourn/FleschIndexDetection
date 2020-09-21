b590d9850c62eb6a402302222c97d1801e314de3
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-21 09:30:06 -0400

    Reading in the file

--- c++/flesch.cpp
+++ c++/flesch.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <fstream>
-
+#include <deque>
 using namespace std;
-
+void readFile(deque<string> &words, string input);
 int main ()
 {
+	string input;
+	cout << "Enter the name of the input file: ";
+	cin >> input;
+
+	deque<string> words(1);
+
+	readFile(words, input);
+	//for (int i = 1; i < words.size(); i++)
+	//	cout << words[i] << endl;
 	return 0;
+}
+void readFile(deque<string> &words, string input)
+{
+    ifstream file;
+    file.open (input);
+    if (!file.is_open()) return;
+
+    string word;
+    while (file >> word)
+    {
+        words.push_back(word);
+    }
 }
