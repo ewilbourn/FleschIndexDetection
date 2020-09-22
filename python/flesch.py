a8811952ec89a6bec6ac32ab99adbc6ba796c4b8
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-22 01:46:03 -0400

    TabError...

--- python/flesch.py
+++ python/flesch.py
@@ -1,22 +1,26 @@
 #Emily Wilbourn
 #Python flesch program
 
-#define functions
+#function to fill a list with all the words from a text file
 def tokenizeFile(fileName):
-  words = []
-  with open(fileName,'r') as file: 
-    # reading each line     
-    for line in file: 
-      # reading each word         
-      for word in line.split(): 
-        # displaying the words            
-        words.append(word) 
-  return words;
+    words = []
+    with open(fileName,'r') as file: 
+        #reading each line     
+        for line in file: 
+      	    #reading each word         
+      	    for word in line.split(): 
+                #if the word isn't a digit, add it to the list           
+                if(not word.isdigit()):
+	  	    words.append(word)
+    return words;
 
 print("Enter the name of the input file: ")
 inputFile = input()
 
+#instantiate a list
 words = []
+
+#fill up the list with the words from the text file
 words = tokenizeFile(inputFile)
-for w in (words):
-	print(w)
+
+
