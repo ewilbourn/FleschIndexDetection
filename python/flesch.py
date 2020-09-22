4e0bc95d67e002ba7342fa024cccf9298840ce90
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-22 01:30:42 -0400

    Reading in file word by word

--- python/flesch.py
+++ python/flesch.py
@@ -1,6 +1,17 @@
 #Emily Wilbourn
 #Python flesch program
 
+#define functions
+def readFile(fileName):
+	with open(fileName,'r') as file: 
+    	  # reading each line     
+     	  for line in file: 
+            # reading each word         
+            for word in line.split(): 
+              # displaying the words            
+              print(word)  
+
+
 print("Enter the name of the input file: ")
 inputFile = input()
-
+readFile(inputFile)
