dd6681ff92badebc5a850c6304a3ed5024680f2b
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-22 01:38:35 -0400

    Storing words in a list

--- python/flesch.py
+++ python/flesch.py
@@ -2,16 +2,21 @@
 #Python flesch program
 
 #define functions
-def readFile(fileName):
-	with open(fileName,'r') as file: 
-    	  # reading each line     
-     	  for line in file: 
-            # reading each word         
-            for word in line.split(): 
-              # displaying the words            
-              print(word)  
-
+def tokenizeFile(fileName):
+  words = []
+  with open(fileName,'r') as file: 
+    # reading each line     
+    for line in file: 
+      # reading each word         
+      for word in line.split(): 
+        # displaying the words            
+        words.append(word) 
+  return words;
 
 print("Enter the name of the input file: ")
 inputFile = input()
-readFile(inputFile)
+
+words = []
+words = tokenizeFile(inputFile)
+for w in (words):
+	print(w)
