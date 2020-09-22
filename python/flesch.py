84ba2e73028a2e71f580e1b4d08b309e3d5528c4
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-22 12:01:30 -0400

    Added method to determine if a char is punctuation

--- python/flesch.py
+++ python/flesch.py
@@ -2,6 +2,8 @@
 #Python flesch program
 
 #function to fill a list with all the words from a text file
+#precondition: pass in the file name (string)
+#postcondition: returns the list of words
 def tokenizeFile(fileName):
 	words = []
 	with open(fileName,'r') as file: 
@@ -11,10 +13,23 @@ def tokenizeFile(fileName):
 					words.append(word) #if the word isn't a digit, add it to the list           
 	return words;
 
-
+#function to count the number of words in a file
+#precondition: pass in the list of words
+#postcondition: return the length of the list of words (integer)
 def totalWords(words):
 	return len(words)
 
+#function to determine if a word has punctuation
+#precondition: pass in the word being stored in a list of strings
+#postcondition: return True if there is punctuat
+def findPunctuation(character):
+	punctuation = {".", ":", ";", "!", "?"}
+	if character in punctuation:
+		return True
+	return False
+		
+
+#####################################################################################
 print("Enter the name of the input file: ")
 inputFile = input()
 
@@ -29,3 +44,5 @@ print("Number of Words: ", numWords)
 
 for w in words:
 	print(w)
+
+
