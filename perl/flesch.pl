3887d80a3908fd358b3b8426bdd9ebc0f187119e
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-23 00:39:11 -0400

    Only adding words (not number) to the array now

--- perl/flesch.pl
+++ perl/flesch.pl
@@ -2,8 +2,11 @@
 use strict;
 use warnings;
 
+
+use Scalar::Util qw(looks_like_number);
+
 #all lines is all the lines in the array; splits the line by spaces and 
-#
+
 
 # Grab the name of the file from the command line, exit if no name given
 my $filename = $ARGV[0] or die "Need to get file name on the command line\n";
@@ -16,15 +19,25 @@ my @all_lines = <DATA>;
 
 #array to hold individual words
 my @all_words = ();
+
+#create a variable that will count the words
+my $word_count = 0;
+
 #Now take each line and break it into tokens based on spaces and print the token
 foreach my $line (@all_lines)
 {
 	my @tokens = split(' ', $line);
 	foreach my $token (@tokens)
 	{
-		push(@all_words, $token);
+		push(@all_words, $token) if(!looks_like_number($token));
+
+		#increment the word counter variable
+		$word_count++ if(!looks_like_number($token));
 	}
 }
 
 #print out the contents of word array
 print "@all_words\n";
+
+#print the word count
+print "$word_count";
