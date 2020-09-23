7b6309700f2dda8a8d13a1d1c02f7bffc3d13a2b
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-09-22 22:07:44 -0400

    Added function to count methods

--- python/flesch.py
+++ python/flesch.py
@@ -23,7 +23,7 @@ def totalWords(words):
 #precondition: pass in the word being stored in a list of strings
 #postcondition: return True if there is punctuat
 def findPunctuation(character):
-	punctuation = {".", ":", ";", "!", "?"}
+	punctuation = {'.', ':', ';', '!', '?'}
 	if character in punctuation:
 		return True
 	return False
@@ -38,6 +38,63 @@ def totalSentences(words):
 			if(findPunctuation(char)):
 				numSentences+=1
 	return numSentences
+
+#function to determine if a character is a vowel
+#precondition: pass in a character
+#postcondition: return True is the character is a vowel, False if it isn't
+def findVowel(character):
+	vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+	if character in vowels:
+		return True
+	return False
+
+#function to count the number of syllables in a word (string)
+#precondition: pass in a word (string)
+#postcondition: return the number of syllables in a word (integer)
+def findSyllables(word):
+	syllables = 0
+	for i in range(len(word)):
+	#this handles when the word ends in an e (which is silent, and thus not a syllable)
+		if(not ((i+1) == len(word) and (word[i]).lower() == 'e')):
+			if findVowel(word[i]):
+				syllables+=1
+				#handles two vowels in a row
+				#i.e. spool, cool, moon
+				if ((i+1)< len(word) and findVowel(word[i+1])):
+					i+=1
+
+	return syllables
+
+#function to count the number of syllables in the file
+#precondition: pass in the list of words (strings)
+#postcondition: return the number of syllables in the text file (integer)
+def totalSyllables(words):
+	numSyllables = 0
+	for word in words:
+		numSyllables += findSyllables(word)	
+	
+	return numSyllables
+
+#function to create a list of Dale Chall words
+#precondition: pass in nothing
+#postcondition: return the list of words (strings)
+def createDaleChallList():
+        daleChall = []
+        daleChall = tokenizeFile("/pub/pounds/CSC330/dalechall/wordlist1995.txt")
+        return daleChall;
+
+#function to count the number of challenging words in a text file based on the Dale Chall List
+#precondition: pass in the list of words (strings)
+#postcondition: return the number of challenging words (integer)
+def countChallengingWords(words):
+	difficultWords = 0
+	daleChall = []
+	daleChall = createDaleChallList()
+	for word in words:
+		 if word in daleChall:
+			 difficultWords+=1
+	return difficultWords
+
 #####################################################################################
 print("Enter the name of the input file: ")
 inputFile = input()
@@ -49,9 +106,13 @@ words = []
 words = tokenizeFile(inputFile)
 numWords = totalWords(words)
 numSentences = totalSentences(words)
+numSyllables = totalSyllables(words)
+numChallWords = countChallengingWords(words)
 
 print("Number of Words: ", numWords)
 print("Number of Sentences: ", numSentences)
+print("Number of Syllables: ", numSyllables)
+print("Number of Challenging Words: ", numChallWords)
 
 for w in words:
 	print(w)
