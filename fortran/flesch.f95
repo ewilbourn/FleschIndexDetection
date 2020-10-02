ab500ac2ffcb0e35b28afbf7203690120903a2ff
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-10-02 12:02:23 -0400

    counting words -- number is too high though...

--- fortran/flesch.f95
+++ fortran/flesch.f95
@@ -1,8 +1,8 @@
 program reader
  implicit none
- character (LEN=5000000) :: long_string
+ character (LEN=5000000) :: long_string, new_long_string
  character (LEN=100) :: input_string 
- integer :: char_counter, sentence_counter
+ integer :: char_counter, sentence_counter, space_counter, i
  character (LEN=1) :: input
 
  interface
@@ -11,7 +11,14 @@ program reader
      character(LEN=1) :: l
      logical:: o
    end function is_sentence
+
+   function is_space (c) result (O)
+     implicit none
+     character(LEN=1) :: c
+     logical :: O
+   end function is_space
  end interface
+
  
  !read in the input file name form the command line
  !i.e. a.out /pub/pounds/CSC330/translations/KJV.txt
@@ -23,9 +30,14 @@ program reader
 
   char_counter=1
   100 read (5,rec=char_counter,err=200) input
+    if (index(input, "#") .ne. 0 ) then
     long_string(char_counter:char_counter) = input
+    endif
     if (is_sentence(input))then
     sentence_counter=sentence_counter+1
+    endif
+    if(is_space(input))then
+    space_counter=space_counter+1
     endif
     char_counter=char_counter+1
     goto 100
@@ -33,10 +45,11 @@ program reader
   char_counter=char_counter-1
   close (5)
 
+
  print *, input_string
  print *, "Read ", char_counter, " characters."
  print *, "Number of Sentences: ", sentence_counter
-
+ print *, "Number of words: ", space_counter+1
  !print *, long_string
 end program reader
 
@@ -47,6 +60,9 @@ logical function is_sentence( letter ) result(out)
   out = .false.
   !punct =  '.:;?!'
 
+  !the index being equal to 0 indicates that something
+  !isn't found, so if the index isn't equal to 0 then
+  !set found equal to true
   if (index(letter,".") .ne. 0) then
     out = .true.
   else if (index(letter,":") .ne. 0) then
@@ -59,3 +75,12 @@ logical function is_sentence( letter ) result(out)
     out = .true.
   endif
 end function is_sentence
+
+logical function is_space (one_char) result (out)
+  character (LEN=1) :: one_char
+  out = .false.
+
+  if (index(one_char, " ") .ne. 0) then
+     out = .true.
+  endif
+end function is_space
