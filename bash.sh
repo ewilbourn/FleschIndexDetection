7e6fb715affb64bff4b71aa10a25e0be84178821
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2020-10-05 22:16:00 -0400

    added commands for all 5 languages on the bash script

--- bash.sh
+++ bash.sh
@@ -30,10 +30,46 @@ java -classpath ./java/ flesch WEB
 java -classpath ./java/ flesch YLT
 
 
+perl ./perl/flesch.pl ASV
+perl ./perl/flesch.pl BBE
+perl ./perl/flesch.pl DARBY
+perl ./perl/flesch.pl KJ21
+perl ./perl/flesch.pl KJV
+perl ./perl/flesch.pl NAB
+perl ./perl/flesch.pl NASB
+perl ./perl/flesch.pl NIV
+perl ./perl/flesch.pl NKJV
+perl ./perl/flesch.pl NLT
+perl ./perl/flesch.pl WEB
+perl ./perl/flesch.pl YLT
 
 
+python3 ./python/flesch.py ASV
+python3 ./python/flesch.py BBE
+python3 ./python/flesch.py DARBY
+python3 ./python/flesch.py KJ21
+python3 ./python/flesch.py NAB
+python3 ./python/flesch.py NASB
+python3 ./python/flesch.py NIV
+python3 ./python/flesch.py NKJV
+python3 ./python/flesch.py NLT
+python3 ./python/flesch.py WEB
+python3 ./python/flesch.py YLT
 
-
+gfortran ./fortran/flesch.f95
+a.out ASV.txt
+a.out BBE.txt
+a.out DARBY.txt
+a.out DRB.txt
+a.out KJ21.txt
+a.out KJV.txt
+a.out NAB.txt
+a.out NASB.txt
+a.out NIV.txt
+a.out NKJV.txt
+a.out NLT.txt
+a.out WEB.txt
+a.out YLT.txt
 
 
 
